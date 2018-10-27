857fab6faf2bb93386497770442fe613a37f528a
Matthew Kennedy <m.kennedy@me.com>
2018-10-27 20:39:01 +0100

    Organise and get some more parts working

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,17 +1,28 @@
 module Spree
   CheckoutController.class_eval do
-    before_action :update_marketing_preferences, only: :update
+    before_action :update_marketing_preferences, :update_terms_preferences, only: :update
 
     def update_marketing_preferences
       return unless params[:state] == 'address'
       @user ||= try_spree_current_user
-      @user.update_attributes(user_gdpr_params) if @user
+      @user.update_attributes(email_marketing) if @user
+    end
+
+    def update_terms_preferences
+      return unless params[:state] == 'payment'
+      @user ||= try_spree_current_user
+      @user.update_attributes(sales_terms) if @user
     end
 
     private
 
-    def user_gdpr_params
+    def email_marketing
       params.require(:user).permit(:accepts_marketing_emails)
     end
+
+    def sales_terms
+      params.require(:user).permit(:accepts_terms_and_conditions)
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Spree::Core::Engine.add_routes do
   put "/update_gdpr", controller: "users", action: "update_gdpr", as: :update_gdpr
   put "/update_marketing_preferences", controller: "checkout", action: "update_marketing_preferences", as: :update_marketing_preferences
+  put "/update_terms_preferences", controller: "checkout", action: "update_terms_preferences", as: :update_terms_preferences
 end
